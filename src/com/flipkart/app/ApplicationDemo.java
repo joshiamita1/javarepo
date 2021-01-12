e85fd29dccfa7b8a0a3fceb2e24e434751167791
amita joshi <amitajoshi.email@gmail.com>
2021-01-12 14:50:35 +0530

    files commited

---
+++ src/com/flipkart/app/ApplicationDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author Joshi_Amita
+ *
+ */
+public class ApplicationDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module flipkartDemo {
+}
