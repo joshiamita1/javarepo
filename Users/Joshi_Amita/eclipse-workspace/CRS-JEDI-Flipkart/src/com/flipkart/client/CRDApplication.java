f4d678a246066ae37589dd2db6f38c303c25ba5b
amita joshi <amitajoshi.email@gmail.com>
2021-01-20 00:03:33 +0530

    add

---
+++ Users/Joshi_Amita/eclipse-workspace/CRS-JEDI-Flipkart/src/com/flipkart/client/CRDApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.client;
+
+/**
+ * @author Joshi_Amita
+ *
+ */
+public class CRDApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
