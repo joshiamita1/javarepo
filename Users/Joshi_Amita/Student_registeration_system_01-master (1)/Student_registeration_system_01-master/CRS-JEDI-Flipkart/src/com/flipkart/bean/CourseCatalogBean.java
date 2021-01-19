d4e93d7f5c282d11e1de895e2aa57a8fdfe45187
amita joshi <amitajoshi.email@gmail.com>
2021-01-19 23:56:48 +0530

    fileuploaded

---
+++ Users/Joshi_Amita/Student_registeration_system_01-master (1)/Student_registeration_system_01-master/CRS-JEDI-Flipkart/src/com/flipkart/bean/CourseCatalogBean.java
@@ -0,0 +1,14 @@
+package com.flipkart.bean;
+
+public class CourseCatalogBean {
+	CourseBean courselist[];
+
+	public CourseBean[] getCourselist() {
+		return courselist;
+	}
+
+	public void setCourselist(CourseBean[] courselist) {
+		this.courselist = courselist;
+	}
+	
+}
---
+++ Users/Joshi_Amita/Student_registeration_system_01-master (1)/Student_registeration_system_01-master/CRS-JEDI-Flipkart/src/com/flipkart/client/StudentClient.java
@@ -0,0 +1,9 @@
+package com.flipkart.client;
+
+public class StudentClient {
+	
+	public void displayMenu() {
+		
+	}
+
+}
