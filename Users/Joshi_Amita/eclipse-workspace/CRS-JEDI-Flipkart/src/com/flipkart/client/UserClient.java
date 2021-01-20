d26d8a8f6aa65d1ca8654867baa59360dd27f5da
amita joshi <amitajoshi.email@gmail.com>
2021-01-20 14:51:14 +0530

    final

---
+++ Users/Joshi_Amita/eclipse-workspace/CRS-JEDI-Flipkart/src/com/flipkart/client/UserClient.java
@@ -0,0 +1,21 @@
+package com.flipkart.client;
+
+
+public class UserClient {
+	public static void main(String[]args) {
+		
+	}
+	public static void showUserMenu() {
+		
+	}
+	public static void register() {
+		
+	}
+	public static void login() {
+		
+	}
+	public static void logout() {
+		
+	}
+
+}
