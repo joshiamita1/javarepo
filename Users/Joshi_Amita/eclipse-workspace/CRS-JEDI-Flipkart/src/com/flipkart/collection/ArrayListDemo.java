3a7c9f223c92618691c4bff60fd44bc5096e5512
amita joshi <amitajoshi.email@gmail.com>
2021-01-20 15:31:53 +0530

    final

---
+++ Users/Joshi_Amita/eclipse-workspace/CRS-JEDI-Flipkart/src/com/flipkart/collection/ArrayListDemo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.flipkart.collection;
+
+import java.util.*;
+
+/**
+ * @author Joshi_Amita
+ *
+ */
+public class ArrayListDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List l1 = new ArrayList();
+		  l1.add("hello");
+		  System.out.println("size of arraylist"+l1.size());
+		  
+		
+	}
+
+}
